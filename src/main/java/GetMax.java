683275c1645c7e89dc86701056ff7e53f7626ac9
Hoa Do <hoa.do.ma@gmail.com>
2023-07-20 17:01:49 +0000

    Completed Challenge: Get Max

--- src/main/java/GetMax.java
+++ src/main/java/GetMax.java
@@ -7,6 +7,18 @@ public class GetMax {
      * @return the largest value in arr.
      */
     public int max(int[] arr){
-        return -1;
+        int largestInt = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if(largestInt < arr[i]){
+                largestInt = arr[i];
+            }
+        }
+        return largestInt;
     }
 }
+
+//set first index to placeholder
+//check placeholder with next index
+//if placeholder lesser than next index, replace placeholder
+//else continue the check
+//terminate at the end of the array
